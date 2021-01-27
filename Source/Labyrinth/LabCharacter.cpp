863957c2add448d61e5269b061565113c2bc65ac
Oscar <oscar.bonduelle@hotmail.fr>
2021-01-27 16:03:23 -0500

    Add LabCharacter blueprint

--- Source/Labyrinth/LabCharacter.cpp
+++ Source/Labyrinth/LabCharacter.cpp
@@ -13,9 +13,6 @@ ALabCharacter::ALabCharacter()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	GetMesh()->SetOwnerNoSee(true);
-	GetMesh()->bCastHiddenShadow = true;
-
 	Vitesse = 0.5f;
 	MaxUseDistance = 800;
 }
--- Source/Labyrinth/LabyrinthGameModeBase.cpp
+++ Source/Labyrinth/LabyrinthGameModeBase.cpp
@@ -2,4 +2,17 @@
 
 
 #include "LabyrinthGameModeBase.h"
+#include "LabCharacter.h"
 
+ALabyrinthGameModeBase::ALabyrinthGameModeBase()
+{
+	// Changer la classe de défaut pour celle définie par le blueprint
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnObject(
+		TEXT("/Game/Blueprints/LabCharacter_BP"));
+
+	if (PlayerPawnObject.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnObject.Class;
+	}
+
+}
