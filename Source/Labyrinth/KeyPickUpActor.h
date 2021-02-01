7af3a43ec912f44fd3112d8ad2f355376684cb73
Yoann Guivarch <yoannkone@gmail.com>
2021-02-01 18:07:08 -0500

    Ajout de la clé sans inventaire

---
+++ Source/Labyrinth/KeyPickUpActor.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickUpActor.h"
+#include "KeyPickUpActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABYRINTH_API AKeyPickUpActor : public APickUpActor
+{
+	GENERATED_BODY()
+};
