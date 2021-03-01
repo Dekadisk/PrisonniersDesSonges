2a88d41b2057b2f941a0e0cab8e47e0ceae39e43
Oscar <oscar.bonduelle@hotmail.fr>
2021-03-01 11:09:15 -0500

    Asset key

--- Source/Labyrinth/KeyPickUpActor.cpp
+++ Source/Labyrinth/KeyPickUpActor.cpp
@@ -2,4 +2,26 @@
 
 
 #include "KeyPickUpActor.h"
+AKeyPickUpActor::AKeyPickUpActor() 
+{
+	KeyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("KeyMesh"));
 
+	KeyMesh->SetupAttachment(MeshComp);
+}
+
+void AKeyPickUpActor::OnEndFocus()
+{
+	if (!bDisableFocus)
+	{
+		// Utilisé par notre PostProcess pour le rendu d'un «surlignage» 
+		MeshComp->SetRenderCustomDepth(false);
+	}
+}
+
+void AKeyPickUpActor::OnBeginFocus() {
+	if (!bDisableFocus)
+	{
+		// Utilisé par notre PostProcess pour le rendu d'un «surlignage»
+		MeshComp->SetRenderCustomDepth(true);
+	}
+}
--- Source/Labyrinth/KeyPickUpActor.h
+++ Source/Labyrinth/KeyPickUpActor.h
@@ -13,4 +13,13 @@ UCLASS()
 class LABYRINTH_API AKeyPickUpActor : public APickUpActor
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* KeyMesh;
+public:
+	AKeyPickUpActor();
+	// Le joueur regarde l'objet
+	virtual void OnBeginFocus() override;
+	// Le joueur arrête de regarder l'objet
+	virtual void OnEndFocus() override;
 };
