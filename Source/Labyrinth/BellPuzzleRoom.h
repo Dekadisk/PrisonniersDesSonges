1cee0a10a1abe9f49bd26b6d26c93afa246203af
Oscar <oscar.bonduelle@hotmail.fr>
2021-03-15 15:55:55 -0400

    BellPuzzle class

---
+++ Source/Labyrinth/BellPuzzleRoom.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PuzzleRoom.h"
+#include "BellPuzzleRoom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABYRINTH_API ABellPuzzleRoom : public APuzzleRoom
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Puzzle")
+	int nbBells;
+
+};
