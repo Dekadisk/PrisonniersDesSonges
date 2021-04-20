dafca3550fddf5a195a7850bc6e34fd87179cad3
Thomas <thomas.fournier@usherbrooke.ca>
2021-04-20 17:05:54 -0400

    Enigme Lampes

---
+++ Source/Labyrinth/LampPuzzleActor.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LampPuzzleActor.h"
+#include "SolvableActor.h"
+
+
+ALampPuzzleActor::ALampPuzzleActor() {
+	Socle = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Socle"));
+	Abat = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Abat"));
+	Tiges = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Tiges"));
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("Light"));
+
+	Socle->AttachTo(MeshComp);
+	Abat->AttachTo(MeshComp);
+	Tiges->AttachTo(MeshComp);
+	Light->AttachTo(MeshComp);
+}
+
+void ALampPuzzleActor::OnConstruction(const FTransform& Transform)
+{
+	Light->SetHiddenInGame(!isOn);
+}
+
+void ALampPuzzleActor::BeginPlay() {
+	if (HasAuthority() && isOn) {
+		for (FPE_PuzzleEventMaster& pem : PuzzleEvents) {
+			if (pem.Trigger == EPuzzleEventCheck::On) {
+				for (FPE_ActorInteractions& ai : pem.Event.Environment.ActorInteractions) {
+					for (AActor* a : ai.Actors) {
+						if (ASolvableActor* sa = Cast<ASolvableActor>(a)) {
+							if (ai.Interaction == EPuzzleEventInteraction::Unlock)
+								sa->Unlock(true);
+							else if(ai.Interaction == EPuzzleEventInteraction::Lock)
+								sa->Lock(true);
+						}
+					}
+				}
+			}
+		}
+	}	
+}
+
+void ALampPuzzleActor::Use(bool Event, APawn* InstigatorPawn) {
+	isOn = !isOn;
+	Light->SetHiddenInGame(!isOn);
+	if (!Event)
+		CheckEvents(EPuzzleEventCheck::Use);
+	if (isOn)
+		CheckEvents(EPuzzleEventCheck::On);
+	else
+		CheckEvents(EPuzzleEventCheck::Off);
+}
---
+++ Source/Labyrinth/LampPuzzleActor.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UsableActor.h"
+#include "Components/PointLightComponent.h"
+#include "LampPuzzleActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABYRINTH_API ALampPuzzleActor : public AUsableActor
+{
+	GENERATED_BODY()
+	
+public:
+
+	ALampPuzzleActor();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* Socle;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* Abat;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* Tiges;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UPointLightComponent* Light;
+
+	UPROPERTY(EditAnywhere, Category = "Lamp")
+	bool isOn;
+
+	virtual void Use(bool Event, APawn* InstigatorPawn = nullptr) override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	void BeginPlay() override;
+};
